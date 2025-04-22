c9e9fd8625ba0d47e439eda0706a16ca3200df4d
JMH2323 <johnmasonhaley23@gmail.com>
2025-04-22 13:52:28 -0500

    PortalWidgetsBase

---
+++ UI/Portal/HTTP/PortalManager.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DedicatedServers/UI/HTTP/HTTPRequestManager.h"
+#include "PortalManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDSERVERS_API UPortalManager : public UHTTPRequestManager
+{
+	GENERATED_BODY()
+
+
+	
+};
---
+++ UI/Portal/SignInOverlay.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SignInOverlay.h"
+
+#include "HTTP/PortalManager.h"
+
+void USignInOverlay::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	check(PortalManagerClass);
+	PortalManager = NewObject<UPortalManager>(PortalManagerClass);
+	
+}
