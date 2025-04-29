f03c3c332ebd766f18150d721d990bd5296d7a6c
JMH2323 <johnmasonhaley23@gmail.com>
2025-04-29 17:05:47 -0500

    ReOrder and Game pt 1/2

---
+++ Game/DS_GameModeBase.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DS_GameModeBase.h"
+
+void ADS_GameModeBase::StartCountdownTimer(FCountdownTimerHandle& CountdownTimerHandle)
+{
+	CountdownTimerHandle.TimerFinishedDelegate.BindWeakLambda(this,[]()
+	{
+		
+	});
+}
+
+void ADS_GameModeBase::StopCountdownTimer(FCountdownTimerHandle& CountdownTimerHandle)
+{
+	
+}
+
+void ADS_GameModeBase::OnCountdownTimerFinished(ECountdownTimerType Type)
+{
+	
+}
