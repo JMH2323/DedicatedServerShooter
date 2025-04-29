dd0f0c8ea52a69e077ac2ee8eb4db432291c4e2e
JMH2323 <johnmasonhaley23@gmail.com>
2025-04-29 16:34:01 -0500

    Timers and Playercontroller

---
+++ Player/DSPlayerController.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "DedicatedServers/Types/DSTypes.h"
+#include "DSPlayerController.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnTimerStateChangedDelegate, float, Time, ECountdownTimerType, Type);
+
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDSERVERS_API ADSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	
+	ADSPlayerController();
+
+	UFUNCTION(Client, Reliable)
+	void Client_TimerUpdated(float CountdownTimeLeft, ECountdownTimerType Type) const;
+
+	UFUNCTION(Client, Reliable)
+	void Client_TimerStopped(float CountdownTimeLeft, ECountdownTimerType Type) const;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnTimerStateChangedDelegate OnTimerUpdated;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnTimerStateChangedDelegate OnTimerStopped;
+
+virtual void ReceivedPlayer() override;
+	
+protected:
+
+	UFUNCTION(Server, Reliable)
+	void Server_Ping(float TimeOfRequest);
+
+	void Client_Pong(float TimeOfRequest);
+	
+private:
+
+	float SingleTripTime;
+	
+};
