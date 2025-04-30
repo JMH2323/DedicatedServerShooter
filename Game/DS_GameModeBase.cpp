474a07f82dbb5c58921e2bf0be58f3e1dff171a8
JMH2323 <johnmasonhaley23@gmail.com>
2025-04-29 19:24:03 -0500

    Pt 2

--- Game/DS_GameModeBase.cpp
+++ Game/DS_GameModeBase.cpp
@@ -3,20 +3,77 @@
 
 #include "DS_GameModeBase.h"
 
+#include "DedicatedServers/Player/DSPlayerController.h"
+
 void ADS_GameModeBase::StartCountdownTimer(FCountdownTimerHandle& CountdownTimerHandle)
 {
-	CountdownTimerHandle.TimerFinishedDelegate.BindWeakLambda(this,[]()
+
+	// When timer is finished, stops countdown
+	CountdownTimerHandle.TimerFinishedDelegate.BindWeakLambda(this,[&]()
 	{
-		
+		StopCountdownTimer(CountdownTimerHandle);
+		OnCountdownTimerFinished(CountdownTimerHandle.Type);
 	});
+
+	// Start timer
+	GetWorldTimerManager().SetTimer(
+		CountdownTimerHandle.TimerFinishedHandle,
+		CountdownTimerHandle.TimerFinishedDelegate,
+		CountdownTimerHandle.CountdownTime,
+		false);
+
+
+
+	// Client Timer is updated by the Update Timer
+	CountdownTimerHandle.TimerUpdateDelegate.BindWeakLambda(this,[&]()
+	{
+		for (FConstPlayerControllerIterator Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
+		{
+			ADSPlayerController* DSPlayerController = Cast<ADSPlayerController>(Iterator->Get());
+			if (IsValid(DSPlayerController))
+			{
+				const float CountdownTimeLeft = CountdownTimerHandle.CountdownTime - GetWorldTimerManager().GetTimerElapsed(CountdownTimerHandle.TimerFinishedHandle);
+				DSPlayerController->Client_TimerUpdated(CountdownTimeLeft, CountdownTimerHandle.Type);
+			}
+		}
+	});
+
+	// Start Update Timer (Goes off every second, updating the client timers)
+	GetWorldTimerManager().SetTimer(
+		CountdownTimerHandle.TimerUpdateHandle,
+		CountdownTimerHandle.TimerUpdateDelegate,
+		CountdownTimerHandle.CountdownUpdateInterval,
+		true);
+	
 }
 
 void ADS_GameModeBase::StopCountdownTimer(FCountdownTimerHandle& CountdownTimerHandle)
 {
+	CountdownTimerHandle.State = ECountdownTimerState::Stopped;
+	GetWorldTimerManager().ClearTimer(CountdownTimerHandle.TimerFinishedHandle);
+	GetWorldTimerManager().ClearTimer(CountdownTimerHandle.TimerUpdateHandle);
+
+	if (CountdownTimerHandle.TimerFinishedDelegate.IsBound())
+	{
+		CountdownTimerHandle.TimerFinishedDelegate.Unbind();
+	}
+	if (CountdownTimerHandle.TimerUpdateDelegate.IsBound())
+	{
+		CountdownTimerHandle.TimerUpdateDelegate.Unbind();
+	}
+
+	for (FConstPlayerControllerIterator Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
+	{
+		ADSPlayerController* DSPlayerController = Cast<ADSPlayerController>(Iterator->Get());
+		if (IsValid(DSPlayerController))
+		{
+			DSPlayerController->Client_TimerStopped(0.f, CountdownTimerHandle.Type);
+		}
+	}
 	
 }
 
 void ADS_GameModeBase::OnCountdownTimerFinished(ECountdownTimerType Type)
 {
-	
+	// Designed to overwrite on child classes when Timer finished
 }
