2b51ff14d5018e480bc8b59c3c8e21ca8b75487c
JMH2323 <johnmasonhaley23@gmail.com>
2025-04-29 21:33:06 -0500

    Lobby Gamemode / Game Instance Sub

---
+++ Game/Instance/DS_GameInstanceSubsystem.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GameLiftServerSDK.h"
+#include "DS_GameInstanceSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDSERVERS_API UDS_GameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UDS_GameInstanceSubsystem();
+	void InitGameLift(const FServerParameters& ServerParams);
+
+	// Gamelift should only be initialized once
+	UPROPERTY(BlueprintReadOnly)
+	bool bGameLiftInitialized;
+
+private:
+	void ParseCommandLinePort(int32& OutPort);
+
+	FProcessParameters ProcessParameters;
+	
+};
