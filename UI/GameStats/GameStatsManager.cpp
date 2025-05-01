c2dbfd7efc88a42f963f361f0ac865dce4c871c7
JMH2323 <johnmasonhaley23@gmail.com>
2025-05-01 14:37:29 -0500

    Http request

--- UI/GameStats/GameStatsManager.cpp
+++ UI/GameStats/GameStatsManager.cpp
@@ -2,3 +2,32 @@
 
 
 #include "GameStatsManager.h"
+
+#include "HttpModule.h"
+#include "JsonObjectConverter.h"
+#include "Interfaces/IHttpRequest.h"
+#include "../HTTP/HTTPRequestTypes.h"
+#include "../../Data/API/APIData.h"
+#include "../../GameplayTags/DedicatedServersTags.h"
+
+void UGameStatsManager::RecordMatchStats(const FDSRecordMatchStatsInput RecordMatchStatsInput)
+{
+
+	
+	// For JsonObject...
+	//	TSharedPtr<FJsonObject> JsonObject = MakeShared<FJsonObject>();
+	//	FJsonObjectConverter::UStructToJsonObject(FDSRecordMatchStatsInput::StaticStruct(), &RecordMatchStatsInput, JsonObject.ToSharedRef());
+
+	// For String...
+	FString JsonString;
+	FJsonObjectConverter::UStructToJsonObjectString(FDSRecordMatchStatsInput::StaticStruct(), &RecordMatchStatsInput, JsonString);
+
+	TSharedRef<IHttpRequest> Request = FHttpModule::Get().CreateRequest();
+	const FString ApiUrl = APIData->GetAPIEndpoint(DedicatedServersTags::GameStatsAPI::RecordMatchStats);
+	Request->SetURL(ApiUrl);
+	Request->SetVerb("POST");
+	Request->SetHeader("Content-Type", "application/json");
+	Request->SetContentAsString(JsonString);
+	Request->ProcessRequest();
+	
+}
